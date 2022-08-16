4abf37acf6f194be8660fc170767736f9f2843bc
kunal <singhkunal98765@gmail.com>
2022-08-16 14:22:42 +0530

    2

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT||5000, () => {
   console.log(`Example app listening at  http://localhost:${port}`);
 });
